95018d1b8afbcc15f140c1ae0519b51d60782417
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-09-05 22:22:49 +0300

    code 18 done

--- 401-code-challange/class-18/README.md
+++ 401-code-challange/class-18/README.md
@@ -2,7 +2,10 @@
 
 create a method in the binary tree class called fizzBuzztree that replace each number divided by 3 with 'Fizz' word, and if it divided by 5 will replaced by 'Buzz', and for 5 and 3 togethor replace with "FizzBuzz". and return the new tree.
 
+## code is in the file called fis=zzbuzztree
+
 
 ## Whiteboard
 
 ![](img/uml.png)
+![](img/fizz2.PNG)
--- 401-code-challange/class-18/__tests__/fizzbuzztree.test.js
+++ 401-code-challange/class-18/__tests__/fizzbuzztree.test.js
@@ -1,46 +1,58 @@
-'use strict';
+"use strict";
+const KaryTree = require("../fizzbuzztree");
+const Node = require("../node");
 
-const fizzBuzz = require('../fizzbuzztree');
-const BinaryTree = require('../BinaryTree');
-const Node=require('../node');
+describe("KaryTree-tests", () => {
+  let secondKaryTree = new KaryTree();
+  let karytreeInstance;
+  beforeAll(() => {
+    let one = new Node(1);
+    let two = new Node(2);
+    let three = new Node(3);
+    let four = new Node(4);
+    let five = new Node(5);
+    let six = new Node(6);
+    let seven = new Node(7);
+    let eight = new Node(8);
+    let nine = new Node(9);
+    let ten = new Node(10);
+    let eleven = new Node(11);
+    let twelve = new Node(12);
+    let thirteen = new Node(13);
+    let fourteen = new Node(14);
+    let fifteen = new Node(15);
+    let sixteen = new Node(16);
+    let seventeen = new Node(17);
 
+    one.children.push(two);
+    one.children.push(three);
+    one.children.push(four);
+    two.children.push(five);
+    two.children.push(six);
+    two.children.push(seven);
+    three.children.push(eight);
+    three.children.push(nine);
+    three.children.push(ten);
+    four.children.push(eleven);
+    four.children.push(twelve);
+    four.children.push(thirteen);
+    five.children.push(fourteen);
+    seven.children.push(fifteen);
+    nine.children.push(sixteen);
+    eleven.children.push(seventeen);
 
-describe('Fizz Buzz Tree', () => {
-
-  // create a new Binary Tree
-  const one = new Node(10);
-  const two = new Node(5);
-  const three = new Node(15);
-  const four = new Node(2);
-  const five = new Node(4);
-  const six = new Node(11);
-  const seven = new Node(15);
-  const eight = new Node(21);
-  const nine = new Node(30);
-
-  const tree = new BinaryTree(one);
-
-  one.left = two;
-  one.right = three;
-  three.left = four;
-  three.right = five;
-  two.left = six;
-  six.right = seven;
-  seven.left = eight;
-  seven.right = nine;
-
-
-  it('check function', () => {
-    expect(fizzBuzz(tree)).toEqual(['buzz','5','11','fizzBuzz','fizz','fizzBuzz','fizzBuzz', 'buzz','4']);
+    karytreeInstance = new KaryTree(one);
   });
+  it("1.Can successfully return a collection from a breadthFisrt traversal", () => {
+    let expectedResults =
+      " 1 >  2 >  3 >  4 >  5 >  6 >  7 >  8 >  9 >  10 >  11 >  12 >  13 >  14 >  15 >  16 >  17 > ";
 
-  it('check function', () => {
-    expect(fizzBuzz()).toEqual(null);
-
+    expect(secondKaryTree.breadthFisrt(karytreeInstance)).toBe(expectedResults);
   });
- 
+  it("2.Can successfully run FizzBuzz method for k-ary tree", () => {
+    let expectedResults =
+      " 1 >  2 >  Fizz >  4 >  Buzz >  Fizz >  7 >  8 >  Fizz >  Buzz >  11 >  Fizz >  13 >  14 >  FizzBuzz >  16 >  17 > ";
 
+    expect(secondKaryTree.fizzBuzztree(karytreeInstance)).toBe(expectedResults);
+  });
 });
-
-
-
